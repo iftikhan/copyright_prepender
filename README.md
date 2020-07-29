6c0b42fb708057fda2b2b58db55278b0888e4c54
Iftikhar ahmed khan <iftkhan@cisco.com>
2020-07-29 15:10:25 +0530

    fix for all ext

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-# copyright_prepender
-Add copyright to files in a directory
+# Add Copyright
+The purpose of this repository is to provide python script i.e `copyright.py` to add copyright content to set of files in a folder/directory.
 
-## Usage:
-    'python copyright.py <dir> --file-extension <.ext>'
 
-## supported_ext
-    '.tf', '.yaml', '.hcl', '.css', '.py', '.yml'
- 
+## How Script Works
+Script take three runtime inputs 
+* file directory we need to add copyright.
+* --copyright-file <copyright.txt> 
+* --file-extension <.ext> : File extension to which you want to add copyright content.
 
-  
+     **_Note: Check copyright content against file .ext, if copyright content  is syntactically compatible with file extension._**
+
+## Usage instructions
+
+To add copyright run `copyright.py` with pram as below:
+ `python copyright.py <dir> --copyright-file <copyright.txt> --file-extension <.ext>`
 
--- copyright.py
+++ copyright.py
@@ -1,78 +1,81 @@
 import os
 import sys
-from typing import List
-import re
 
 
 def main():
-    args: List[str] = sys.argv[1:]
-    supported_ext = ['.tf', '.yaml', '.hcl', '.css', '.py', '.yml']
+    args = sys.argv[1:]
 
     if not args:
-        print('usage: python copyright.py <dir> --file-extension <.ext>')
+        print('usage: python copyright.py <dir> --copyright-file <copyright.txt> --file-extension <.ext>')
         print('To insert copyright in files.')
         sys.exit(1)
 
     directory = args[0]
 
-    if len(args) != 3:
-        print("Please provide all required param i.e copyright.py <dir> --file-extension <.ext>")
+    if len(args) != 5:
+        print(
+            "Please provide all required param i.e "
+            "copyright.py <dir> --copyright-file <copyright.txt> --file-extension <.ext>")
         sys.exit(1)
 
-    if args[1] != '--file-extension':
-        print("Please provide all required param i.e copyright.py <dir> --file-extension <.ext>")
+    if args[1] != '--copyright-file':
+        print(
+            "Please provide all required param i.e "
+            "copyright.py <dir> --copyright-file <copyright.txt> --file-extension <.ext>")
+        sys.exit(1)
+
+    # Get copyright text
+    copyright_file = args[2]
+
+    if args[3] != '--file-extension':
+        print(
+            "Please provide all required param i.e "
+            "copyright.py <dir> --copyright-file <copyright.txt> --file-extension <.ext>")
         sys.exit(1)
 
     # Get extension
-    ext = args[2]
-
-    if ext not in supported_ext:
-        print(f"{ext} extension is not supported yet. Supported ext are {supported_ext}.")
-        sys.exit(1)
+    ext = args[4]
 
     # get the content of directory.
     files = os.listdir(directory)
 
     if len(files) == 0:
-        print(f"No file found with provided extension {ext}")
+        print("No file found with extension " + ext)
         sys.exit(1)
 
     if not os.path.isdir(directory):
         print('Not a valid directory!!!')
         print('usage: python copyright.py <dir> --file-extension <.tf>')
         sys.exit(1)
-    
-    # TODO: Plase update the regex as per new copytight content
-    pattern = re.compile("^/?#+\n+#+ Copyright \(c\) [0-9]{4}-[0-9]{4} by Cisco Systems, Inc\.")
-    try:
 
-        fp = open('copyright.txt', 'r')
+    try:
+        fp = open(copyright_file, 'r')
         copyright_text = ''.join(fp.readlines())
         fp.close()
-        add_copyright(directory, ext, pattern, copyright_text)
-    except Exception as ex:
+        add_copyright(directory, ext, copyright_text)
+    except FileNotFoundError as er:
         print("Please add 'copyright.txt' file with Copyright content in it.")
         sys.exit(1)
 
 
-def add_copyright(directory, ext, pattern, copyright_text):
+def add_copyright(directory, ext, copyright_text):
     obj = os.listdir(directory)
     for f in obj:
         if os.path.isdir(directory + '/' + f):
-            add_copyright(directory + '/' + f, ext, pattern, copyright_text)
+            add_copyright(directory + '/' + f, ext, copyright_text)
         # operate only files
         if os.path.isfile(directory + '/' + f) and f.endswith(ext):
             fp = open(directory + '/' + f, 'r')
             lines = fp.readlines()
             fp.close()
-            if not pattern.match(''.join(lines)):
+            if copyright_text not in (''.join(lines)):
                 lines.insert(0, ''.join(copyright_text) + '\n')
                 fp = open(directory + '/' + f, 'r+')
                 fp.writelines(lines)
                 fp.close()
-                print(f'Copyright added in {f}')
+                print("Copyright added in " + f)
             else:
-                print(f"Copyright already exist in {directory + '/' + f}")
+                print("Copyright already exist in " + directory + "/" + f)
 
 
 if __name__ == '__main__':
